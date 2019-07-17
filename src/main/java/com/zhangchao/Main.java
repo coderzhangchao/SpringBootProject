d34c95e37c4263786ed0f5f6d1a6ae21c0147a76
fansan <15388657853@163.com>
2019-07-17 23:54:17 +0800

    2019-07-17

--- src/main/java/com/zhangchao/Main.java
+++ src/main/java/com/zhangchao/Main.java
@@ -3,6 +3,18 @@ package com.zhangchao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+    *   标注在某个类上，表示这是一个Spring Boot的配置类
+ * @SpringBootConfiguration
+ * 
+ * @EnableAutoConfiguration
+ * @ComponentScan(excludeFilters = {
+ * 	@Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
+ * 	@Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class) 
+ * })
+ * 
+ */
 @SpringBootApplication
 public class Main {
 
