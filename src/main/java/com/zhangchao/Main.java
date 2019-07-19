ddb6493fc19cdeac0ee55a63123d626b9a349dbf
fansan <15388657853@163.com>
2019-07-19 23:33:17 +0800

    2019-07-19

--- src/main/java/com/zhangchao/Main.java
+++ src/main/java/com/zhangchao/Main.java
@@ -1,5 +1,7 @@
 package com.zhangchao;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -20,7 +22,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
 
+	private final static Logger logger = LoggerFactory.getLogger(Main.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
+		// 测试项目日志
+		logger.info("项目启动成功..........");
 	} 
 }
